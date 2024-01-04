f579f19321c6f862a6184e92ffa4b3839e41bf1a
subin1126 <subink1126@gmail.com>
2024-01-04 17:20:03 +0900

    elice

---
+++ 10, 11주차 - 자바스크립트 클론코딩/완성본/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './app';
+import YoutubeService from './components/service/youtubeService';
+
+const youtubeService = new YoutubeService(process.env.REACT_APP_YOUTUBE_API_KEY);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App youtubeService={youtubeService}/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
